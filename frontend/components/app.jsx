b6fe5b349ff87c67c994a3c86d0753fc6f764f23
pdeguzman <padgzmn@gmail.com>
2022-08-12 16:24:37 -0400

    created root, app and entry file

---
+++ frontend/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header>
+        <h1>Welcome to YourBnB!</h1>
+      </Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/components/root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import App from './app';
+
+const Root = () => {
+  return (
+    <App />
+  );
+}
+
+export default Root;
+
