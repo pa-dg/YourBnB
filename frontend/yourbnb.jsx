fef803d64b1b8f885abbbc4f5f6a52cbbbecd953
pdeguzman <padgzmn@gmail.com>
2022-08-15 12:45:35 -0400

    updated entry point

--- frontend/yourbnb.jsx
+++ frontend/yourbnb.jsx
@@ -6,7 +6,23 @@ import Root from './components/root';
 import { signup, login, logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } 
+  else {
+    store = configureStore();
+  }
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store}/>, root);
 
   // TO REMOVE, FOR TESTING ONLY
   window.getState = store.getState;
@@ -16,7 +32,4 @@ document.addEventListener('DOMContentLoaded', () => {
   window.logout = logout;
   window.signup = signup;
   // END
-
-  const root = document.getElementById('root');
-  ReactDOM.render(<Root store={store}/>, root);
 });
