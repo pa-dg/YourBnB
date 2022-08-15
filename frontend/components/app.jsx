f4c65bb8024b4d14d51aa1d0d36b579397cfd4f4
pdeguzman <padgzmn@gmail.com>
2022-08-14 21:19:50 -0400

    updated root and app components

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className="app">
+    <div className="app-container">
       <Header>
         <h1>Welcome to YourBnB!</h1>
       </Header>
--- frontend/yourbnb.jsx
+++ frontend/yourbnb.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import { signup, login, logout } from './util/session_api_util';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
+
+  // TO REMOVE, FOR TESTING ONLY
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-
-  // window.signup = signup;
-  // window.login = login;
-  // window.logout = logout;
+  // END
 
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to YourBnB</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
