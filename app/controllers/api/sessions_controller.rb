cc6742020dfeedbab6eb3b0223f247cb0904a20f
pdeguzman <padgzmn@gmail.com>
2022-10-27 12:42:31 -0400

    edit backend error for user login

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,13 +3,13 @@ class Api::SessionsController < ApplicationController
     @user = User.find_by_credentials(
       params[:user][:email],
       params[:user][:password]
-      )
+    )
 
     if @user
       login_user!(@user)
       render "api/users/show"
     else
-      render json: ["Invalid email/password combination"], status: 401
+      render json: ["Invalid credentials"], status: 401
     end
   end
 
