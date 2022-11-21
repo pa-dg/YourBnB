13f39a99f661b2c56565e6789220de725cd1b3e7
pdeguzman <padgzmn@gmail.com>
2022-11-21 01:25:54 -0500

    update styling on search bar input

--- frontend/components/search/SearchForm.jsx
+++ frontend/components/search/SearchForm.jsx
@@ -1,42 +1,35 @@
 import React, { useState } from "react";
 
-const SearchForm = ({ city, updateFilter }) => {
+const SearchForm = ({ updateFilter }) => {
   const [filter, setFilter] = useState({ city: "" })
-  const [isLoading, setIsLoading] = useState(false);
 
   const update = (e) => {
     setFilter({city: e.currentTarget.value})
-    console.log('#', filter)
   }
   
   const handleSubmit = (e) => {
     e.preventDefault();
     updateFilter(filter)
-    setIsLoading(true)
   }
-  
-  console.log('updatefilter', updateFilter)
-  console.log('state', filter)
+
   return (
     <form className="search-bar-container" onSubmit={handleSubmit}>
       <div className="search-input">
         <label htmlFor="search-input">
           <input 
+            title="Try New York"
             type="text" 
-            placeholder= 'Search by City (New York..)'
+            placeholder="Search by City (New York..)"
             value={filter.city}
             onChange={update}
           />
         </label>
-        
       </div>
       <button className="search-button">
         <i className="fas fa-search fa-1x"></i>
       </button>
     </form>
-
   )
-
 }
 
 export default SearchForm;
